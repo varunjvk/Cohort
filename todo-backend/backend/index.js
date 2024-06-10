1d9d2be2e4a75ed035f09752807f58ff26c80d61
Varun <jvkumar560@outlook.com>
2024-06-10 15:07:40 +0530

    Done backend

--- todo-backend/backend/index.js
+++ todo-backend/backend/index.js
@@ -1,25 +1,46 @@
 const express = require('express')
 const { createTodo, updateTodo } = require('./types')
+const { todo } = require('./db')
 const app = express()
 app.use(express.json())
 
-app.post('/todos', function (req, res) {
+app.post('/todos', async function (req, res) {
     const { success, data } = createTodo.safeParse(req.body)
     if (success) {
+        await todo.create({ ...data, completed: false })
+        res.json({
+            "msg":"Todo is created"
+        })
+    }
+    else {
+        res.status(411).json({
+            "msg":"Your input formats are wrong"
+        })
+        return
+    }
+})
+
+app.get('/todos', async function (req, res) {
+    const todos = await todo.find({})
+    res.json({
+        todos
+    })``
+})
+
+app.put('/completed', async function (req, res) {
+    const { success, data } = updateTodo.safeParse(req.body)
+    if (success) {
+        await todo.update({ _id: data.id }, { completed: true })
         
     }
     else {
         res.status(411).json({
-            "msg":"Your input formats are worng"
+            "msg":"You sent wrong inputs"
         })
     }
-})
-
-app.get('/todos', function (req, res) {
     
 })
 
-app.put('/completed', function (req, res) {
-    const {success, data} = updateTodo.safeParse(req.body)
-    
+app.listen(3000, () => {
+    console.log("Listening");
 })
